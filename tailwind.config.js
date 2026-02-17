e0165b6421c9b33cab358956832f70dc1559e7bb
tilok-wp <tilokpaul.web@gmail.com>
2026-02-17 19:06:49 +0600

    Update header menu

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#4F39F6',
+                dark: '#1E2939',
+            },
+        }
+    }
+}
